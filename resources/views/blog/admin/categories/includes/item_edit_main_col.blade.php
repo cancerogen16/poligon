f0cdbfde911a7478a7fddb35293565964b407ff1
cancerogen <don544@mail.ru>
2021-05-13 00:31:14 +0500

    {{ old('description', $item->description) }}

--- resources/views/blog/admin/categories/includes/item_edit_main_col.blade.php
+++ resources/views/blog/admin/categories/includes/item_edit_main_col.blade.php
@@ -27,7 +27,7 @@
                 </div>
                 <div class="form-group">
                     <label for="description" class="form-label">Описание</label>
-                    <textarea id="description" name="description" class="form-control" rows="3">{{ $item->description }}</textarea>
+                    <textarea id="description" name="description" class="form-control" rows="3">{{ old('description', $item->description) }}</textarea>
                 </div>
             </div>
         </div>
