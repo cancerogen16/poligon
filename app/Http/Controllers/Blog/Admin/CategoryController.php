6d96e41fc0f6a4fa19e4fd176f83378978bf86b2
cancerogen <don544@mail.ru>
2021-05-12 22:39:09 +0500

    Список категорий в админке с пагинацией

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Blog\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\BlogCategory;
 use Illuminate\Http\Request;
 
 class CategoryController extends BaseController
@@ -14,7 +14,9 @@ class CategoryController extends BaseController
      */
     public function index()
     {
-        dd(__METHOD__);
+        $paginator = BlogCategory::paginate(5);
+
+        return view('blog.admin.category.index', compact('paginator'));
     }
 
     /**
